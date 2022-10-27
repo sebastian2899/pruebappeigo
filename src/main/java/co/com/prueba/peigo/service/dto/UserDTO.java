062805bc9af092aa28106f2c887ac7045748e960
Harold Steven  Taborda Gutiérrez <harold.taborda@mercadolibre.com.co>
2022-10-27 14:56:26 -0500

    Update UserDTO.java

--- src/main/java/co/com/prueba/peigo/service/dto/UserDTO.java
+++ src/main/java/co/com/prueba/peigo/service/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package co.com.prueba.peigo.service.dto;
 
+import co.com.prueba.peigo.domain.User;
 import lombok.Data;
 
 /**
@@ -28,6 +29,12 @@ public class UserDTO {
 
     public UserDTO() {
         // Empty constructor needed for Jackson.
+    }
+    
+    public UserDTO(User user) {
+        this.id = user.getId();
+        // Customize it here if you need, or not, firstName/lastName/etc
+        this.login = user.getLogin();
     }
 
    
